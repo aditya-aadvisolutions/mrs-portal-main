a2692a5c493f0119304b3a48da72402f132a79d5
Aditya Reddy <aditya@aadvisolutions.com>
2024-08-14 16:09:26 +0530

    added dev config

--- src/components/upload/uppyupload.tsx
+++ src/components/upload/uppyupload.tsx
@@ -32,6 +32,7 @@ export default function UppyUpload(props: any) {
   const [modalFile, setModalFile] = useState<any>('');
 
   const uploadUrl = import.meta.env.VITE_S3_URL;
+  console.log('Uppy uploadUrl..',uploadUrl);
   const Role = localStorage.getItem("authentication") ? JSON.parse(localStorage.getItem("authentication") as string).roleName : "";
   const { fileNames } = useLocation().state || { fileNames: [] };
 
